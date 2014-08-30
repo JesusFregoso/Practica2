ac45e24563091d9b35c95d64ed7259871ddada93
jessfregoso0@gmail.com <j20121993>
2014-08-29 20:03:36 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php echo "hola mundo esto es php";?>
+
+<html>
+	<head><tittle>Deploy en heroku</tittle>
+	</head>
+	<body>
+		<p>Mi primer proyecto en heroku + github</p>
+	<?php> echo "La suma de 5 + 5 =".(5 + 5);?>
+	</body>
+</html>
